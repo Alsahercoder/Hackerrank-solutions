63d12745dedf0d543df1e6ac41e00c973f49d094
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2021-03-30 19:47:21 +0530

    Create Lab8.3_PostOrder.py

---
+++ Lab8.3_PostOrder.py
@@ -0,0 +1,8 @@
+# Source: https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
+def postOrder(root):
+    
+    if root != None:
+        
+        postOrder(root.left)
+        postOrder(root.right)
+        print(root.info, end = " ")
