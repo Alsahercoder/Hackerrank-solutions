de94cddada22e532bfa50b5b518952d8e905d54e
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2021-03-30 19:45:34 +0530

    Create Lab8.2_PreOrder.py

---
+++ Lab8.2_PreOrder.py
@@ -0,0 +1,7 @@
+# Source: https://www.hackerrank.com/challenges/tree-preorder-traversal/problem
+def preOrder(root):
+    if root != None:
+        
+        print(root.info, end = " ")
+        preOrder(root.left)
+        preOrder(root.right)
