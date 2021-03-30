c0fc370b5ce4cf6e9added98166bc28ef6ecc410
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2021-03-30 19:48:27 +0530

    Create Lab8.4_PostOrder.py

---
+++ Lab8.4_PostOrder.py
@@ -0,0 +1,8 @@
+#Source: https://www.hackerrank.com/challenges/tree-inorder-traversal/problem
+def inOrder(root):
+    
+    if root != None:
+        
+        inOrder(root.left)
+        print(root.info, end = " ")
+        inOrder(root.right)
