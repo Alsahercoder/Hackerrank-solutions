813ea9a005e003d650cc15346a18f67623c16fee
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2021-03-30 19:42:18 +0530

    Update Lab8.1_BSTInsertion.py

--- Lab8.1_BSTInsertion.py
+++ Lab8.1_BSTInsertion.py
@@ -4,35 +4,31 @@
 #self.right (the right child of the node)
 #self.info (the value of the node)
 
-    def insert(self, val):
-                
-        if self.root == None:
-            
-            self.root = Node(val)
-        
-        else:
-            ptr = self.root
-            parentPtr = ptr
-            while ptr != None:
-                
-                if val < ptr.info:
-                    
-                    parentPtr = ptr
-                    ptr = ptr.left
-                elif val >= ptr.info:
-                    
-                    parentPtr = ptr
-                    ptr = ptr.right
-            
-            if val < parentPtr.info:
-                
-                parentPtr.left = Node(val)
-            
-            else:
-                parentPtr.right = Node(val)
-        
-        return self.root
-                
-        
-        #Enter you code here.
+def insert(self, val):
 
+    if self.root == None:
+
+        self.root = Node(val)
+
+    else:
+        ptr = self.root
+        parentPtr = ptr
+        while ptr != None:
+
+            if val < ptr.info:
+
+                parentPtr = ptr
+                ptr = ptr.left
+            elif val >= ptr.info:
+
+                parentPtr = ptr
+                ptr = ptr.right
+
+        if val < parentPtr.info:
+
+            parentPtr.left = Node(val)
+
+        else:
+            parentPtr.right = Node(val)
+
+    return self.root
